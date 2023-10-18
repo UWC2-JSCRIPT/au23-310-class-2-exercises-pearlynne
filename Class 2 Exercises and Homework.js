2652f10eaaf4eae41cbad933b23bb2b9f4539619
Pearlynne <chongpearlynnelh@gmail.com>
2023-10-17 20:50:21 -0700

    Update in-class Questions 1 - 5

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -5,8 +5,22 @@
 // - 'favorite food'
 // - bestFriend (object with the same 3 properties as above)
 
+// Create object representation of self without bestFriend object
+const pearlC = {
+	firstName: 'Pearl',
+	lastName: 'C',
+	'favorite food': 'sushi',
+	bestFriend: {
+		firstName: 'Janie',
+		lastName: 'Doe',
+		'favorite food': 'salami',
+	},
+};
+
 
 // 2. console.log best friend's firstName and your favorite food
+
+console.log(`My best friend's first name is ${pearlC.bestFriend['firstName']} and my favorite food is ${pearlC['favorite food']}.`);
 
 
 // 3. Create an array to represent this tic-tac-toe board
@@ -14,12 +28,25 @@
 // -XO
 // X-X
 
+const ticTacToe =
+	[['-', 'O', '-'],
+	['-', 'X', 'O'],
+	['X', '-', 'X']];
+
 
 // 4. After the array is created, 'O' claims the top right square.
 // Update that value.
 
+ticTacToe[0][2] = 'O'
+
 
 // 5. Log the grid to the console.
+
+console.log(ticTacToe[0])
+console.log(ticTacToe[1])
+console.log(ticTacToe[2])
+console.table(ticTacToe)
+
 
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
@@ -30,32 +57,40 @@
 // - Use regexp test method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
 
 
+
+
+
+
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
 // Convert this string to a Date
 const assignmentDate = '1/21/2019';
 
 
+
+
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
+
+
 
 
 // 9. Use dueDate values to create an HTML time tag in format
 // <time datetime="YYYY-MM-DD">Month day, year</time>
 // I have provided a months array to help
 const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
 ];
 
 
